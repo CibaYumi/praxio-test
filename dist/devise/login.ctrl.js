29d68fd881d543d4c8e9fa4122c980f83e9f3ea3
Cibele Yumi <contato@cibeleyumi.com.br>
2019-02-14 09:31:42 -0200

    WIP - Fix password icon to show/hide pass, add models and create angular module to login function

--- dist/devise/login.ctrl.js
+++ dist/devise/login.ctrl.js
@@ -1,6 +1,31 @@
 (function(){
   'use strict';
-  angular.module('FrontTest').controller('loginCtrl', function ($scope) {
+  angular.module('FrontTest').controller('loginCtrl', function ($scope, $location, $rootScope, App, Auth) {
+
+    if(Auth.session.get()) $location.path('/home');
+
+    $scope.login = {};
+
+    $rootScope.load = [];
+    App.load.start();
+
+    $scope.logIn = function () {
+      App.load.start('Validando seu usuário...');
+      Auth.login($scope.login)
+        .then(function (result) {
+          try {
+            // App.load.stop();
+            Auth.session.set(result);
+            $location.path('/home');
+          } catch (e) {
+            alert('Erro ao salvar sua sessão, tente novamente!');
+          }
+        }).catch(function (err) {
+          // App.load.stop('Erro ao efetuar o login!');
+          // alert('Erro ao logar!');
+          console.log(err);
+        });
+    };
 
   });
 })();
