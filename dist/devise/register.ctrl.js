bb1759e5262840685ac50ff9a04c1ae362549bf3
Cibele Yumi <contato@cibeleyumi.com.br>
2019-02-14 09:32:28 -0200

    WIP - Fix password icon to show/hide pass, add models and create angular module to register function

--- dist/devise/register.ctrl.js
+++ dist/devise/register.ctrl.js
@@ -1,6 +1,19 @@
 (function(){
   'use strict';
-  angular.module('FrontTest').controller('registerCtrl', function ($scope) {
+  angular.module('FrontTest').controller('registerCtrl', function ($scope, Auth) {
+
+    $scope.newUser = {};
+
+    $scope.register = function () {
+      Auth.create($scope.newUser)
+        .then(function (userCreated) {
+          alert('Criou!');
+          console.log(userCreated);
+        }).catch(function (err) {
+          console.log(err);
+          alert('Pau!');
+        });
+    }
 
   });
 })();
