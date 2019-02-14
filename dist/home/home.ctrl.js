dc69ecafa5167a038b03f6a0f91be953b7c66279
Cibele Yumi <contato@cibeleyumi.com.br>
2019-02-14 12:05:01 -0200

    WIP - Create logout function

--- dist/home/home.ctrl.js
+++ dist/home/home.ctrl.js
@@ -1,6 +1,18 @@
 (function(){
   'use strict';
-  angular.module('FrontTest').controller('homeCtrl', function ($scope) {
+  angular.module('FrontTest').controller('homeCtrl', function ($scope, $location, Auth) {
+
+    if(!Auth.session.get()) $location.path('/login');
+
+    $scope.logout = function () {
+      try {
+        Auth.logout();
+        $location.path('/login');
+      } catch (e) {
+        alert('Erro ao deslogar!');
+      }
+    };
+
 
   });
 })();
